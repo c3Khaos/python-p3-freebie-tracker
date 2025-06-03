579e6308bfaed9198a6d0cfa0f9aa39e16721047
christopherchege <chrisshege35@gmail.com>
2025-06-04 00:17:58 +0300

    defined models,configure alembic,create migrations andimplement core methods

--- lib/models.py
+++ lib/models.py
@@ -1,30 +1,57 @@
-from sqlalchemy import ForeignKey, Column, Integer, String, MetaData
+
+from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.orm import relationship, backref
 from sqlalchemy.ext.declarative import declarative_base
 
-convention = {
-    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
-}
-metadata = MetaData(naming_convention=convention)
-
-Base = declarative_base(metadata=metadata)
+Base = declarative_base()
 
 class Company(Base):
     __tablename__ = 'companies'
 
-    id = Column(Integer(), primary_key=True)
-    name = Column(String())
-    founding_year = Column(Integer())
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    founding_year = Column(Integer)
 
-    def __repr__(self):
-        return f'<Company {self.name}>'
+    freebies = relationship('Freebie', backref='company')
+
+    @property
+    def devs(self):
+        return list({freebie.dev for freebie in self.freebies})
+
+    def give_freebie(self, dev, item_name, value):
+        return Freebie(item_name=item_name, value=value, dev=dev, company=self)
+
+    @classmethod
+    def oldest_company(cls, session):
+        return session.query(cls).order_by(cls.founding_year).first()
 
 class Dev(Base):
     __tablename__ = 'devs'
 
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
 
-    id = Column(Integer(), primary_key=True)
-    name= Column(String())
+    freebies = relationship('Freebie', backref='dev')
 
-    def __repr__(self):
-        return f'<Dev {self.name}>'
+    @property
+    def companies(self):
+        return list({freebie.company for freebie in self.freebies})
+
+    def received_one(self, item_name):
+        return any(f.item_name == item_name for f in self.freebies)
+
+    def give_away(self, dev, freebie):
+        if freebie in self.freebies:
+            freebie.dev = dev
+
+class Freebie(Base):
+    __tablename__ = 'freebies'
+
+    id = Column(Integer, primary_key=True)
+    item_name = Column(String)
+    value = Column(Integer)
+    company_id = Column(Integer, ForeignKey('companies.id'))
+    dev_id = Column(Integer, ForeignKey('devs.id'))
+
+    def print_details(self):
+        return f"{self.dev.name} owns a {self.item_name} from {self.company.name}"
--- lib/seed.py
+++ lib/seed.py
@@ -1,3 +1,26 @@
 #!/usr/bin/env python3
+from models import Company, Dev, Freebie
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine
+
+engine = create_engine('sqlite:///freebies.db')
+Session = sessionmaker(bind=engine)
+session = Session()
+
+c1 = Company(name="Apple", founding_year=1976)
+c2 = Company(name="Google", founding_year=1998)
+
+d1 = Dev(name="Alice")
+d2 = Dev(name="Bob")
+
+session.add_all([c1, c2, d1, d2])
+session.commit()
+
+f1 = c1.give_freebie(d1, "Sticker", 1)
+f2 = c2.give_freebie(d1, "T-shirt", 10)
+f3 = c2.give_freebie(d2, "Water Bottle", 5)
+
+session.add_all([f1, f2, f3])
+session.commit()
 
 # Script goes here!
